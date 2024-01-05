1d5d1e88a6673e3edb6322b4791b836fbdfeb451
caremperador <filtrosperu@gmail.com>
2024-01-05 16:25:14 -0500

    correccion en el modelo categorias sobre realaciones

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     use HasFactory;
+    public function listas()
+    {
+        return $this->hasMany(Lista::class);
+    }
+
+    public function videos()
+    {
+        return $this->belongsToMany(Video::class, 'video_categoria');
+    }
 }
