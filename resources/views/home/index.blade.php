71f7c3a9788278dc426f7ce3fc527137cc0f4817
caremperador <filtrosperu@gmail.com>
2024-02-16 11:51:56 -0500

    Actualizar el diseño de la descripción del video en la página de inicio

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -16,7 +16,7 @@
                         <!-- div izquierda -->
                         <div class="flex flex-col justify-center p-10 items-center z-20 text-justify text-white w-1/2">
                             <h2 class="text-base md:text-2xl lg:text-3xl xl:text-4xl font-bold mb-3 tracking-tighter">{{ $video->titulo }}</h2>
-                            <p class="text-xs md:text-base lg:text-lg sm:hidden mb-5">
+                            <p class="text-xs md:text-base lg:text-lg sm:hidden md:block mb-5">
                                 {{ Str::limit($video->descripcion, 100, '...') }}
                             </p>
                             <div class="flex items-center space-x-2">
