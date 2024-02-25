27704f9312a77f01a26fc288ae1d437e69acdfd1
caremperador <filtrosperu@gmail.com>
2024-02-24 22:22:56 -0500

    Actualizar estilos de aviso en blade.php

--- resources/views/layouts/aviso.blade.php
+++ resources/views/layouts/aviso.blade.php
@@ -1,5 +1,5 @@
 
-<div class="bg-blue-600 fixed z-70" id="alert-message">
+<div class="bg-blue-600" id="alert-message">
     <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
       <div class="flex items-center justify-between flex-wrap">
         <div class="w-0 flex-1 flex items-center">
