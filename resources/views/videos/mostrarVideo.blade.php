35fa05222bf5478548701a9551b623484f4ae1a8
caremperador <filtrosperu@gmail.com>
2024-02-26 22:58:09 -0500

    Actualizar vista de mostrarVideo.blade.php***
    ***Actualizar vista de mostrarVideo.blade.php

--- resources/views/videos/mostrarVideo.blade.php
+++ resources/views/videos/mostrarVideo.blade.php
@@ -5,46 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video en Pantalla Completa</title>
     @vite('resources/css/app.css')
-    @include('scripts.pa_antiadblock_7142069')
 </head>
-
 <body class="bg-gray-900">
-
     <!-- Iframe normal para navegadores web -->
-    <iframe id="normal-iframe" class="hidden absolute top-0 left-0 w-full h-full border-none"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+    <iframe id="normal-iframe" src="{{ $videoUrl }}" class="absolute top-0 left-0 w-full h-full border-none" allowfullscreen>
         Tu navegador no soporta iframes.
     </iframe>
-
     <!-- Contenedor para Iframe específico de WebView de Android -->
     <div id="iframe-container" class="fixed inset-0 flex justify-center items-center bg-gray-900" style="display: none;">
-        <iframe id="webview-iframe" class="w-full h-full p-20 border-none" allowfullscreen>
+        <iframe id="webview-iframe" src="{{ $videoUrl }}" class="w-full h-full border-none" allowfullscreen>
             Tu navegador no soporta iframes.
         </iframe>
     </div>
-
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             var userAgent = navigator.userAgent.toLowerCase();
             var isAndroidWebView = userAgent.includes('wv');
-
-            // Obtén referencias al contenedor del iframe y al iframe normal
             var normalIframe = document.getElementById('normal-iframe');
             var iframeContainer = document.getElementById('iframe-container');
             var webviewIframe = document.getElementById('webview-iframe');
 
-            // Configura el src del iframe apropiado según el contexto
-            var videoSrc = "{{ $videoUrl }}";
             if (isAndroidWebView) {
-                webviewIframe.src = videoSrc;
+                // Elimina el iframe normal y muestra el contenedor del iframe específico para WebView
+                normalIframe.parentNode.removeChild(normalIframe);
                 iframeContainer.style.display = 'flex';
             } else {
-                normalIframe.src = videoSrc;
-                normalIframe.style.display = 'block';
+                // Elimina el contenedor del iframe específico para WebView
+                webviewIframe.parentNode.removeChild(webviewIframe);
             }
         });
     </script>
-
     <img src="https://whos.amung.us/widget/yaskevideos.png" style="display:none" />
     @vite('resources/js/app.js')
 </body>
