6ed59e9139cdb3a86e3d7cdbf308bfe67748ddd4
caremperador <filtrosperu@gmail.com>
2024-02-26 21:01:28 -0500

    Actualizar diseño de visualización de video en pantallas pequeñas

--- resources/views/videos/mostrarVideo.blade.php
+++ resources/views/videos/mostrarVideo.blade.php
@@ -15,9 +15,11 @@
 </iframe>
 
 <!-- Este iframe se muestra solo en pantallas menores a md -->
-<iframe src="{{ $videoUrl }}" class="block md:hidden absolute top-0 left-0 w-[200px] h-[200px] border-none">
+<div class="absolute inset-0 flex items-center justify-center  border">
+<iframe src="{{ $videoUrl }}" class="block md:hidden w-[90%] h-[90%] border-none">
     Tu navegador no soporta iframes.
 </iframe>
+</div>
 
     <img src="https://whos.amung.us/widget/yaskevideos.png" style="display:none" />
     <!-- Incluir app.js al final del body -->
