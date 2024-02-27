a6441e9bf06760ff07d808d08cd55c9c848c9bf1
caremperador <filtrosperu@gmail.com>
2024-02-26 20:54:53 -0500

    Agrega soporte para mostrar diferentes iframes según el tamaño de la pantalla

--- resources/views/videos/mostrarVideo.blade.php
+++ resources/views/videos/mostrarVideo.blade.php
@@ -8,9 +8,17 @@
     
 </head>
 <body class=" bg-gray-900">
-    <iframe src="{{ $videoUrl }}" class="absolute top-0 left-0 w-full h-full border-none">
-        Tu navegador no soporta iframes.
-    </iframe>
+
+    <!-- Este iframe se muestra en pantallas md hacia arriba -->
+<iframe src="{{ $videoUrl }}" class="hidden md:block absolute top-0 left-0 w-full h-full border-none">
+    Tu navegador no soporta iframes.
+</iframe>
+
+<!-- Este iframe se muestra solo en pantallas menores a md -->
+<iframe src="{{ $videoUrl }}" class="block md:hidden absolute top-0 left-0 w-[200px] h-[200px] border-none">
+    Tu navegador no soporta iframes.
+</iframe>
+
     <img src="https://whos.amung.us/widget/yaskevideos.png" style="display:none" />
     <!-- Incluir app.js al final del body -->
     @vite('resources/js/app.js')
