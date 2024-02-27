ea3e5da99532d216f08e5a3405efe88e13594d5d
caremperador <filtrosperu@gmail.com>
2024-02-26 21:35:26 -0500

    Actualizar la visualización de videos en pantallas y WebView

--- resources/views/videos/mostrarVideo.blade.php
+++ resources/views/videos/mostrarVideo.blade.php
@@ -5,21 +5,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video en Pantalla Completa</title>
     @vite('resources/css/app.css')
-    
 </head>
 <body class=" bg-gray-900">
 
-    <!-- Este iframe se muestra en pantallas md hacia arriba -->
-<iframe src="{{ $videoUrl }}" class="hidden md:block absolute top-0 left-0 w-full h-full border-none">
-    Tu navegador no soporta iframes.
-</iframe>
+    <!-- Iframe para navegadores normales y WebView -->
+    <iframe id="normal-iframe" src="{{ $videoUrl }}" class="absolute top-0 left-0 w-full h-full border-none" style="display: none;">
+        Tu navegador no soporta iframes.
+    </iframe>
 
-<!-- Este iframe se muestra solo en pantallas menores a md -->
-<div class="absolute inset-0 flex items-center justify-center  border">
-<iframe src="{{ $videoUrl }}" class="block md:hidden w-[90%] h-[90%] border-none">
-    Tu navegador no soporta iframes.
-</iframe>
-</div>
+    <!-- Iframe específico para WebView de Android -->
+    <iframe id="webview-iframe" src="{{ $videoUrl }}" class="absolute top-0 left-0 w-[200px] h-[200px] border-none" style="display: none;">
+        Tu navegador no soporta iframes.
+    </iframe>
+
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var userAgent = navigator.userAgent.toLowerCase();
+            var isAndroidWebView = userAgent.includes('wv');
+
+            // Obtén referencias a ambos iframes
+            var normalIframe = document.getElementById('normal-iframe');
+            var webviewIframe = document.getElementById('webview-iframe');
+
+            if (isAndroidWebView) {
+                // Muestra el iframe específico para WebView y oculta el normal
+                webviewIframe.style.display = 'block';
+            } else {
+                // Muestra el iframe normal y oculta el específico para WebView
+                normalIframe.style.display = 'block';
+            }
+        });
+    </script>
 
     <img src="https://whos.amung.us/widget/yaskevideos.png" style="display:none" />
     <!-- Incluir app.js al final del body -->
