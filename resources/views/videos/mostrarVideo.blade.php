5edbb3b6b4112c58ccb638699e1a9c7e3a43487b
caremperador <filtrosperu@gmail.com>
2024-02-26 22:47:44 -0500

    Actualizar la visualización de videos en mostrarVideo.blade.php

--- resources/views/videos/mostrarVideo.blade.php
+++ resources/views/videos/mostrarVideo.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -12,15 +11,14 @@
 <body class="bg-gray-900">
 
     <!-- Iframe normal para navegadores web -->
-    <iframe id="normal-iframe" src="{{ $videoUrl }}" class="hidden absolute top-0 left-0 w-full h-full border-none"
+    <iframe id="normal-iframe" class="hidden absolute top-0 left-0 w-full h-full border-none"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         Tu navegador no soporta iframes.
     </iframe>
 
     <!-- Contenedor para Iframe específico de WebView de Android -->
     <div id="iframe-container" class="fixed inset-0 flex justify-center items-center bg-gray-900" style="display: none;">
-
-        <iframe id="webview-iframe" src="{{ $videoUrl }}" class="w-full h-full p-20 border-none" allowfullscreen>
+        <iframe id="webview-iframe" class="w-full h-full p-20 border-none" allowfullscreen>
             Tu navegador no soporta iframes.
         </iframe>
     </div>
@@ -33,12 +31,15 @@
             // Obtén referencias al contenedor del iframe y al iframe normal
             var normalIframe = document.getElementById('normal-iframe');
             var iframeContainer = document.getElementById('iframe-container');
+            var webviewIframe = document.getElementById('webview-iframe');
 
             if (isAndroidWebView) {
-                // Muestra el contenedor del iframe específico para WebView y oculta el iframe normal
+                // Configura el src del iframe para WebView y muestra el contenedor
+                webviewIframe.src = "{{ $videoUrl }}";
                 iframeContainer.style.display = 'flex';
             } else {
-                // Muestra el iframe normal
+                // Configura el src del iframe normal y lo muestra
+                normalIframe.src = "{{ $videoUrl }}";
                 normalIframe.style.display = 'block';
             }
         });
@@ -47,5 +48,4 @@
     <img src="https://whos.amung.us/widget/yaskevideos.png" style="display:none" />
     @vite('resources/js/app.js')
 </body>
-
 </html>
