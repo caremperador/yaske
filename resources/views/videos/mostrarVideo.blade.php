1eaca63db173c090ff358020fda77d6d5f3d47f7
caremperador <filtrosperu@gmail.com>
2024-02-26 20:32:29 -0500

    Actualizar estilos de pantalla completa en mostrarVideo.blade.php

--- resources/views/videos/mostrarVideo.blade.php
+++ resources/views/videos/mostrarVideo.blade.php
@@ -5,27 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Video en Pantalla Completa</title>
     @vite('resources/css/app.css')
-    <style>
-        body, html {
-            margin: 0;
-            padding: 0;
-            height: 100%;
-            overflow: hidden;
-        }
-        .fullscreen-iframe {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            border: none;
-        }
-    </style>
-
     @include('scripts.pa_antiadblock_7142069')
 </head>
 <body class="m-0 p-0 md:h-screen overflow-hidden bg-gray-900">
-    <iframe src="{{ $videoUrl }}" class="absolute top-0 left-0 w-full h-full border-none" allowfullscreen>
+    <iframe src="{{ $videoUrl }}" class="absolute top-0 left-0 w-full h-full border-none">
         Tu navegador no soporta iframes.
     </iframe>
     <img src="https://whos.amung.us/widget/yaskevideos.png" style="display:none" />
