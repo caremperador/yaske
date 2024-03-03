38ae94f1d49a9b2211ee821e82ec2b9657ef5888
caremperador <filtrosperu@gmail.com>
2024-03-03 00:24:43 -0500

    Actualizar estilos y colores en el formulario de creación de capítulos

--- resources/views/videos/crear_capitulos.blade.php
+++ resources/views/videos/crear_capitulos.blade.php
@@ -11,10 +11,10 @@
             {{-- Selector de Lista --}}
             <div class="mb-3">
                 <label for="lista_id" class="form-label">Seleccionar Lista</label>
-                <select name="lista_id" id="lista_id" class="form-select">
+                <select style="color: black;" name="lista_id" id="lista_id" class="form-select">
                     <option value="">Seleccione una Lista</option>
                     @foreach ($listas as $lista)
-                        <option value="{{ $lista->id }}">{{ $lista->titulo }}</option>
+                        <option style="color: black;" value="{{ $lista->id }}">{{ $lista->titulo }}</option>
                     @endforeach
                 </select>
             </div>
@@ -22,21 +22,21 @@
             {{-- Título del Capítulo --}}
             <div class="mb-3">
                 <label for="titulo" class="form-label">Título del Capítulo</label>
-                <input type="text" name="titulo" id="titulo" class="form-control" required>
+                <input style="color: black;" type="text" name="titulo" id="titulo" class="form-control" required>
             </div>
 
             {{-- URL del Video --}}
             <div class="mb-3">
                 <label for="url_video" class="form-label">URL del Video</label>
-                <input type="text" name="url_video" id="url_video" class="form-control" required>
+                <input style="color: black;" type="text" name="url_video" id="url_video" class="form-control" required>
             </div>
 
             {{-- Estado --}}
             <div class="mb-3">
                 <label for="estado" class="form-label">Estado</label>
                 <select name="estado" id="estado" class="form-select">
-                    <option value="1">Activo</option>
-                    <option value="0">Inactivo</option>
+                    <option style="color: black;" value="1">Activo</option>
+                    <option style="color: black;" value="0">Inactivo</option>
                 </select>
             </div>
 
