4ba3b148969c69a17782d43a860e8b599e6b81a5
caremperador <filtrosperu@gmail.com>
2024-03-03 23:51:12 -0500

    Agregado código para contador de tráfico en el sitio web

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -467,6 +467,9 @@
 
     @stack('scripts')
     <img src="https://whos.amung.us/widget/yaskeros.png" style="display:none" />
+    <div class="hidden" style="display:none">
+        <script type="text/javascript" src="https://cdn.livetrafficfeed.com/static/v5/live.js?bc=ffffff&tc=000000&brd1=2853a8&lnk=135d9e&hc=ffffff&hfc=2853a8&nc=19ff19&vv=210&tft=10&ro=0&tz=America%2FNew_York&res=0"></script><noscript id="LTF_live_website_visitor"><a href="http://livetrafficfeed.com">Website Traffic Counter</a></noscript>
+    </div>
 </body>
 
 </html>
