ea116cb52860e6599d413075ddfc1a5a040436c2
caremperador <filtrosperu@gmail.com>
2024-03-13 14:47:03 -0500

    Agregados saltos de línea en el formulario de creación de capítulos

--- resources/views/videos/crear_capitulos.blade.php
+++ resources/views/videos/crear_capitulos.blade.php
@@ -69,7 +69,7 @@
                     rows="4"></textarea>
             </div>
             <h2> links premium</h2>
-            <hr>
+            <hr><br>
             <div class="mb-6">
                 <label for="url_video" class="block mb-2 text-sm font-medium">URL del Video (Inglés)</label>
                 <input type="text" name="url_video" id="url_video"
@@ -95,7 +95,7 @@
             </div>
             {{-- links gratis --}}
             <h2> links gratis</h2>
-            <hr>
+            <hr><br>
             <div class="mb-6">
                 <label for="url_video_gratis" class="block mb-2 text-sm font-medium">URL del Video (Inglés) gratis</label>
                 <input type="text" name="url_video_gratis" id="url_vide_gratiso"
